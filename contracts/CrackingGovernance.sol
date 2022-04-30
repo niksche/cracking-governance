aed4f97b677f0712b76232118c685d55cf58201a
Nikita Schelkanov <nik.schelkanov@gmail.com>
2022-04-30 12:14:01 +0300

    moving all files from old machine

--- contracts/CrackingGovernance.sol
+++ contracts/CrackingGovernance.sol
@@ -2,22 +2,110 @@
 
 pragma solidity >=0.8.0 <=0.9.0;
 
+import "hardhat/console.sol";
+
+
+// understand how to handle money storage
+// for example someone sends me money, where does contract put it at the firts place
+
 contract CrackingGovernance {
     uint256 private creationTimestamp;
     string private msgToCaller;
+    address private owner;
+
+
+    struct Election {
+        uint256 creationTimestamp;
+        mapping(address => uint) voteCounter;
+        mapping(address => bool) voters;
+        uint depositedEthAmount;
+        address currentLider;
+        uint currentMaxVotes;
+        bool active;
+        uint totalVotes;
+    }
+
+    uint private numberOfElection;
+
+    mapping (uint => Election) public elections;
 
     constructor() {
+        owner = msg.sender;
         creationTimestamp = block.timestamp;
-        msgToCaller = "DERJY ROYAL";
     }
 
-    function getRoyalties(uint256 minutesSince)
-        public
-        view
-        returns (string memory heh)
-    {
-        assert(block.timestamp > creationTimestamp + minutesSince * 1 minutes);
-        heh = msgToCaller;
-        return heh;
+    function newGovernanceElection() public returns (uint electionID) {
+        electionID = numberOfElection++;
+        Election storage e = elections[electionID];
+        e.creationTimestamp = block.timestamp;
+        e.depositedEthAmount = 0 wei;
+        e.active = true;
     }
+
+    function getELection(uint electionID) public view returns (uint256, uint, address, uint) {
+        Election storage e  =  elections[electionID];
+        return (e.creationTimestamp, e.depositedEthAmount, e.currentLider, e.currentMaxVotes);
+    }
+
+    function getNumbersOfElection() public view returns (uint) {
+        return numberOfElection;
+    }
+
+    function vote(uint electionID, address candidate) public payable {
+        Election storage e = elections[electionID];
+        assert(e.active == true);
+        assert(e.voters[msg.sender] == false);
+        // if (block.timestamp > auctionEndTime)
+        //     revert AuctionAlreadyEnded();
+        
+        assert(msg.value == 10000000000000000 wei);
+
+        if (e.currentMaxVotes < ++e.voteCounter[candidate]) {
+            e.currentLider = candidate;
+            e.currentMaxVotes = e.voteCounter[candidate];
+        }
+
+        e.voters[msg.sender] = true;
+        e.depositedEthAmount += 10000000000000000 wei;
+        e.totalVotes++;
+        console.log("heh", e.depositedEthAmount);
+    }
+
+    function getElectionDetail(uint electionID) public view returns (bool, uint256, uint, address, uint, uint) {
+        Election storage e = elections[electionID];
+        return (e.active, e.creationTimestamp, e.depositedEthAmount, e.currentLider, e.currentMaxVotes, e.totalVotes);
+    }
+
+
+    function withdrawComission(uint electionID) public payable {
+        require(msg.sender == owner);
+        Election storage e = elections[electionID];
+        require(e.active == false);
+        uint weiAmountComission = e.depositedEthAmount / 10;
+        payable(owner).transfer(weiAmountComission);
+    }
+
+
+    function finishGovernanceElection(uint electionID) public payable returns (address) {
+        Election storage e = elections[electionID];
+        require(block.timestamp > e.creationTimestamp + 0 minutes);
+
+        address winner = e.currentLider;
+        // TODO: add safemath for calculation 0.9 from deposited amount
+        console.log("full deposit: ", e.depositedEthAmount);
+        uint weiAmountToWinner = e.depositedEthAmount * 9 / 10;
+
+        console.log("full deposit: ", weiAmountToWinner);
+
+        e.active = false;
+
+        console.log("balance before: ", address(winner).balance);
+
+        // check whether that works or not;
+        payable(winner).transfer(weiAmountToWinner);
+
+        return winner;
+
+    }
+
 }
--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,78 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("GovernanceCracking", function () {
+  it("Should set electionsCount to zerro once contract is created", async function () {
+    const CrackingGovernance = await ethers.getContractFactory("CrackingGovernance");
+    const cracker = await CrackingGovernance.deploy();
+    await cracker.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    expect(await cracker.getNumbersOfElection()).to.equal("0");
+    
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    const createGovernanceVotingTx = await cracker.newGovernanceElection();
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    expect(createGovernanceVotingTx.value).to.equal(0);
+    
+    const [owner, firstacc, secondacc] = await ethers.getSigners();
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    let getELectionState = await cracker.getELection(0)
+
+    // console.log(heh);
+    // console.log("election map is: ",getELectionState)
+
+
+    const voteTx = await cracker.connect(owner).vote(0, secondacc.address,  { value: ethers.utils.parseEther("0.01") });
+
+    // console.log("owner: ",owner);
+    // console.log("address: ",owner.address);
+
+    const voteTx2 = await cracker.connect(firstacc).vote(0, secondacc.address,  { value: ethers.utils.parseEther("0.01") });
+
+    // const voteTx3 = await cracker.connect(owner).vote(0, firstacc.address,  { value: ethers.utils.parseEther("0.01") });
+    // const voteTx4 = await cracker.connect(secondacc).vote(0, secondacc.address,  { value: ethers.utils.parseEther("0.01") });
+
+
+
+    let heh  = await voteTx.wait();
+    await voteTx2.wait();
+    // await voteTx3.wait();
+
+    // await voteTx4.wait();
+    
+
+    getELectionState = await cracker.getELection(0)
+
+    // console.log(heh);
+
+    // TODO: add methods to publicly get data on governance auction 
+    // expect().to.equal("fwef");
+
+
+    const callForEnd = await cracker.connect(firstacc).finishGovernanceElection(0);
+
+    const fwefe = await callForEnd.wait();
+    // console.log('declare the winner: ',fwefe);
+
+    // expect(fwefe).to.equal(secondacc.address);
+
+    console.log('before:',await owner.getBalance());
+
+    const comissionTx = await cracker.connect(owner).withdrawComission(0);
+
+    console.log("after",await owner.getBalance());
+
+
+    const electionDetailsTx = await cracker.connect(owner).getElectionDetail(0);
+    console.log(electionDetailsTx);
+
+
+
+    // const voteTx4 = await cracker.connect(secondacc).vote(0, secondacc.address,  { value: ethers.utils.parseEther("0.01") });
+
+
+
   });
+
+  
 });
