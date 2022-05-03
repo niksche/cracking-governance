0722cbb93c99b309d71489440a6933824f85fb44
Nikita Schelkanov <nik.schelkanov@gmail.com>
2022-05-03 17:50:44 +0300

    refactor rest for contract

--- test/index.ts
+++ test/index.ts
@@ -1,83 +1,67 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { CrackingGovernance, CrackingGovernance__factory } from "../typechain";
+// import { ethers } from "ethers";
+// import { ethers } from "hardhat";
 
-describe("GovernanceCracking", function () {
+describe("Cracking governance contract", function () {
+  let CrackingGovernanceFactory;
+  let CrackingGovernance: CrackingGovernance;
+  let owner: SignerWithAddress;
+  let account1: SignerWithAddress;
+  let account2: SignerWithAddress;
+  let accounts: SignerWithAddress[];
+
+  beforeEach(async function () {
+    CrackingGovernanceFactory = await ethers.getContractFactory(
+      "CrackingGovernance"
+    );
+    [owner, account1, account2, ...accounts] = await ethers.getSigners();
+    CrackingGovernance = await CrackingGovernanceFactory.deploy();
+  });
+
+  // You can nest describe calls to create subsections.
   describe("Deployment", function () {
+    it("Should set electionsCount to zerro once contract is created", async function () {
+      expect(await CrackingGovernance.getNumbersOfElection()).to.equal("0");
+    });
 
   });
 
+  describe("addVoting", function () {
+    it("The first created voting returns id, which is equal to zero also total numbers of election increased by 1", async function () {
+      const createGovernanceVotingTx = await CrackingGovernance.addVoting();
+      expect(createGovernanceVotingTx.value).to.equal(0);
+      expect(await CrackingGovernance.getNumbersOfElection()).to.equal("1");
+    });
+  })
 
-  it("Should set electionsCount to zerro once contract is created", async function () {
-    const CrackingGovernance = await ethers.getContractFactory("CrackingGovernance");
-    const cracker = await CrackingGovernance.deploy();
-    await cracker.deployed();
+  describe("vote", function () {
+    it("do something that give me hint on state changing", async function () {
+      const createGovernanceVotingTx = await CrackingGovernance.addVoting();
 
-    expect(await cracker.getNumbersOfElection()).to.equal("0");
-    
+      const voteTx = await CrackingGovernance.connect(owner).vote(0, account1.address,  { value: ethers.utils.parseEther("0.01") });
+      const voteTx2 = await CrackingGovernance.connect(account1).vote(0, account2.address,  { value: ethers.utils.parseEther("0.01") });
 
-    const createGovernanceVotingTx = await cracker.addVoting();
+      // TODO: localize the test for vote function only
+    })
+  })
 
-    expect(createGovernanceVotingTx.value).to.equal(0);
-    
-    const [owner, firstacc, secondacc] = await ethers.getSigners();
+  describe("finish", function() {
+    it("finishes the elction, all tokens are transfered according to the result", async function() {
+      const createGovernanceVotingTx = await CrackingGovernance.addVoting();
 
-    let getELectionState = await cracker.getElection(0)
-
-    // console.log(heh);
-    // console.log("election map is: ",getELectionState)
-
-
-    const voteTx = await cracker.connect(owner).vote(0, secondacc.address,  { value: ethers.utils.parseEther("0.01") });
-
-    // console.log("owner: ",owner);
-    // console.log("address: ",owner.address);
-
-    const voteTx2 = await cracker.connect(firstacc).vote(0, secondacc.address,  { value: ethers.utils.parseEther("0.01") });
-
-    // const voteTx3 = await cracker.connect(owner).vote(0, firstacc.address,  { value: ethers.utils.parseEther("0.01") });
-    // const voteTx4 = await cracker.connect(secondacc).vote(0, secondacc.address,  { value: ethers.utils.parseEther("0.01") });
+      const voteTx = await CrackingGovernance.connect(owner).vote(0, account1.address,  { value: ethers.utils.parseEther("0.01") });
+      const voteTx2 = await CrackingGovernance.connect(account1).vote(0, account2.address,  { value: ethers.utils.parseEther("0.01") });
+      const callForEnd = await CrackingGovernance.connect(account1).finish(0);
+      const comissionTx = await CrackingGovernance.connect(owner).withdrawComission(0);
 
 
-
-    let heh  = await voteTx.wait();
-    await voteTx2.wait();
-    // await voteTx3.wait();
-
-    // await voteTx4.wait();
-    
-
-    getELectionState = await cracker.getElection(0)
-
-    // console.log(heh);
-
-    // TODO: add methods to publicly get data on governance auction 
-    // expect().to.equal("fwef");
-
-
-    const callForEnd = await cracker.connect(firstacc).finish(0);
-
-    const fwefe = await callForEnd.wait();
-    // console.log('declare the winner: ',fwefe);
-
-    // expect(fwefe).to.equal(secondacc.address);
-
-    console.log('before:',await owner.getBalance());
-
-    const comissionTx = await cracker.connect(owner).withdrawComission(0);
-
-    console.log("after",await owner.getBalance());
-
-
-    const electionDetailsTx = await cracker.connect(owner).getElectionDetail(0);
-    console.log(electionDetailsTx);
+      const fwefe = await callForEnd.wait();
+    })
+  })
 
 
 
-    // const voteTx4 = await cracker.connect(secondacc).vote(0, secondacc.address,  { value: ethers.utils.parseEther("0.01") });
-
-
-
-  });
-
-  
 });
